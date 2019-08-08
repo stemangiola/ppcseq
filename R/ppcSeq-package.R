5fab2184506e0d87c7ff152511cff1b17e8c7b07
stemangiola <mangiolastefano@gmail.com>
2019-08-08 20:58:08 +1000

    update files
    Former-commit-id: bb442d658ac8d4dc7de9aa5cdcf031039eed9596

--- R/ppcSeq-package.R
+++ R/ppcSeq-package.R
@@ -11,6 +11,6 @@
 #' @importFrom rstan sampling
 #'
 #' @references
-#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. https://mc-stan.org
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
 #'
 NULL
