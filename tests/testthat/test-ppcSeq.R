2719de42f7cec36d49c3becc92e42d734ae95edc
stemangiola <mangiolastefano@gmail.com>
2019-09-10 15:37:31 +1000

    trying to reduce memory consumption

--- tests/testthat/test-ppcSeq.R
+++ tests/testthat/test-ppcSeq.R
@@ -6,18 +6,18 @@ test_that("dummy",expect_equal(1,1))
 #
 # test_that("Quick test",{
 #
-  # FDR_threshold = 0.01
-  #
-  # res =
-  #   ppcSeq::counts %>%
-  #   mutate(is_significant = FDR < FDR_threshold) %>%
-  #   ppc_seq(
-  #     formula = ~ Label,
-  #     significance_column = PValue,
-  #     do_check_column  = is_significant,
-  #     value_column = value,
-  #     percent_false_positive_genes = "5%", tol_rel_obj = 0.01
-  #   )
+  FDR_threshold = 0.01
+
+  res =
+    ppcSeq::counts %>%
+    mutate(is_significant = FDR < FDR_threshold) %>%
+    ppc_seq(
+      formula = ~ Label,
+      significance_column = PValue,
+      do_check_column  = is_significant,
+      value_column = value,
+      percent_false_positive_genes = "5%", tol_rel_obj = 0.01
+    )
 #
 #   expect_equal(
 #
