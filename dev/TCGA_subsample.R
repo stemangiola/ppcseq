47a9a98606b79bb243bf4dc7e4c4ef9a6b6f7d25
stemangiola <mangiolastefano@gmail.com>
2020-01-06 10:18:23 +1100

    update development scripts
    Former-commit-id: d5c8331faa3c4fb52423c349cd31fbac3a2a67bf

---
+++ dev/TCGA_subsample.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(ppcSeq)
+
+TCGA_tbl_subsample %>%
+	mutate(do_check = (!`house keeping`) & run==1) %>%
+	mutate(`read count` = `read count` %>% as.integer) %>%
+	ppc_seq(
+		significance_column = PValue,
+		do_check_column = do_check,
+		value_column = `read count`,
+		sample_column = sample,
+		gene_column = transcript,
+		percent_false_positive_genes = "1%",
+		tol_rel_obj = 0.01,
+		cores = 2,
+		approximate_posterior_inference = T,
+		approximate_posterior_analysis = T,
+		pass_fit = F
+	)
+
+
+system.time({
+
+TCGA_tbl_subsample %>%
+	inner_join(
+		(.) %>% distinct(sample) %>% slice(1:20)
+	) %>%
+	select(-run) %>%
+	left_join(
+		(.) %>%
+			distinct(transcript) %>%
+			mutate(run = sample(1:40, replace = T, size=n()))
+	) %>%
+
+	mutate(do_check = (!`house keeping`) & run==1) %>%
+	mutate(`read count` = `read count` %>% as.integer) %>%
+	ppc_seq(
+		significance_column = PValue,
+		do_check_column = do_check,
+		value_column = `read count`,
+		sample_column = sample,
+		gene_column = transcript,
+		percent_false_positive_genes = "1%",
+		tol_rel_obj = 0.01,
+		cores = 2,
+		approximate_posterior_inference = T,
+		approximate_posterior_analysis = T,
+		pass_fit = F
+	)
+})
