e938b88232f098afebe8699e7510b7740ba84c67
stemangiola <mangiolastefano@gmail.com>
2020-05-29 17:09:08 +1000

    fix deleterious test + other scripts for fig 5

--- dev/calibration_false_negative.R
+++ dev/calibration_false_negative.R
@@ -3,7 +3,7 @@ library(magrittr)
 library(ppcseq)
 library(furrr)
 plan(multiprocess)
-
+library(patchwork)
 
 my_theme =
 	theme_bw() +
@@ -41,10 +41,10 @@ FDR_threshold = 0.2
 #
 # res_1 %>% saveRDS("dev/res_1_for_calibration_false_negative.rds")
 
-res_1 = readRDS("dev/res_1_for_calibration_false_negative.rds")
-
-outlier_prop = 1e-10
-how_many_outliers = 0.2
+# res_1 = readRDS("dev/res_1_for_calibration_false_negative.rds")
+#
+# outlier_prop = 1e-10
+# how_many_outliers = 0.2
 
 # outlier_df =
 # res_1 %>%
@@ -116,7 +116,7 @@ input_2 =
 	mutate(value = value %>% as.integer) %>%
 	mutate(is_significant = FDR < FDR_threshold)
 
-saveRDS(input_2, "input_2.rds", compress = "gzip")
+saveRDS(input_2, "dev/input_2.rds", compress = "gzip")
 
 es =
 	expand.grid(
@@ -140,7 +140,9 @@ es =
 					 		.do_check = is_significant,
 					 		percent_false_positive_genes = sprintf("%s%%", .x),
 					 		cores = 30,
-					 		approximate_posterior_inference = F, approximate_posterior_analysis = F
+					 		approximate_posterior_inference = F,
+					 		approximate_posterior_analysis = F,
+					 		do_check_only_on_detrimental = F
 					 	)
 			)
 	)
@@ -168,15 +170,15 @@ es_1_step_model =
 					 		.do_check = is_significant,
 					 		percent_false_positive_genes = sprintf("%s%%", .x),
 					 		cores = 30,
-					 		approximate_posterior_inference = F, approximate_posterior_analysis = F
+					 		do_check_only_on_detrimental = F
 					 	)
 			)
 	)
 
 es_1_step_model %>% saveRDS("dev/es_calibration_false_negative_1_step_model.rds")
 
-# es = readRDS("dev/es_calibration_false_negative.rds")
-# es_1_step_model = readRDS("dev/es_calibration_false_negative_1_step_model.rds")
+es = readRDS("dev/es_calibration_false_negative.rds")
+es_1_step_model = readRDS("dev/es_calibration_false_negative_1_step_model.rds")
 
 es_to_stats = function(es){
 	es %>%
@@ -222,76 +224,99 @@ es_to_stats = function(es){
 				pull(n) %>% sum
 		)
 		) %>%
-		mutate(TP_rate = TP/real_pos, FP_rate = FP/real_neg)
+		mutate(TP_rate = TP/real_pos, FP_rate = FP/real_neg) %>%
+
+		# False positive frequency
+		mutate(P = map_int(
+			data,
+			~ .x %>%
+				count(outlier_detected = `ppc samples failed` > 0) %>%
+				filter(outlier_detected ) %>%
+				pull(n) %>% when(length(.)==0 ~ 0L, ~ (.))
+		)
+		) %>%
+
+		mutate(FP_over_P = FP_rate / P)
 }
 stats = es_to_stats(es)
 stats_1_step_model = es_to_stats(es_1_step_model)
 
 
-fp_stat =
-	stats %>%
-	unnest(data) %>%
-	filter(is_symbol_outlier == 1) %>%
-	mutate(false_positive =   `ppc samples failed` > 0 ) %>%
-	mutate(true_negative =  `ppc samples failed` == 0) %>%
-	group_by(fp, run) %>%
-	summarise(sum(false_positive), sum(true_negative)) %>%
-	mutate(`false positive predicted` = `sum(false_positive)` / (`sum(false_positive)` + `sum(true_negative)`))
-
-fn_stat =
-	stats %>%
-	unnest(data) %>%
-	filter(is_symbol_outlier > 1) %>%
-	mutate(true_positive =   `ppc samples failed` > 0 ) %>%
-	mutate(false_negative =  `ppc samples failed` == 0) %>%
-	group_by(fp, run) %>%
-	summarise(sum(true_positive), sum(false_negative)) %>%
-	mutate(`false negative predicted` = `sum(false_negative)` / (`sum(false_negative)` + `sum(true_positive)`))
-
-
-p1 =
-	fp_stat %>%
-	ggplot(aes(x = fp/100, y = `false positive predicted` )) +
-	geom_smooth(method = "lm", color = "#4b68b1") +
-	geom_point() +
-	#geom_abline(intercept = 0, slope = 1, linetype ="dashed") +
-	xlab("False positive aimed") +
-	ylab("False positive predicted") +
-	my_theme
-
-
-p2 =
-	fn_stat %>%
-	ggplot(aes(x = fp / 100, y = `false negative predicted` )) + geom_point()  +
-	geom_abline(intercept = 0, slope = 1) +
-	xlab("False positive aimed") +
-	ylab("False negative predicted") +
-	my_theme
+# fp_stat =
+# 	stats %>%
+# 	unnest(data) %>%
+# 	filter(is_symbol_outlier == 1) %>%
+# 	mutate(false_positive =   `ppc samples failed` > 0 ) %>%
+# 	mutate(true_negative =  `ppc samples failed` == 0) %>%
+# 	group_by(fp, run) %>%
+# 	summarise(sum(false_positive), sum(true_negative)) %>%
+# 	mutate(`false positive predicted` = `sum(false_positive)` / (`sum(false_positive)` + `sum(true_negative)`))
+#
+# fn_stat =
+# 	stats %>%
+# 	unnest(data) %>%
+# 	filter(is_symbol_outlier > 1) %>%
+# 	mutate(true_positive =   `ppc samples failed` > 0 ) %>%
+# 	mutate(false_negative =  `ppc samples failed` == 0) %>%
+# 	group_by(fp, run) %>%
+# 	summarise(sum(true_positive), sum(false_negative)) %>%
+# 	mutate(`false negative predicted` = `sum(false_negative)` / (`sum(false_negative)` + `sum(true_positive)`))
+#
+#
+# p1 =
+# 	stats %>%
+# 	ggplot(aes(x = fp/100, y = FP_over_P )) +
+# 	geom_smooth(method = "lm", color = "#4b68b1") +
+# 	geom_point() +
+# 	geom_abline(intercept = 0, slope = 1, linetype ="dashed") +
+# 	xlab("False positive aimed") +
+# 	ylab("False positive predicted") +
+# 	my_theme
+#
+#
+# p2 =
+# 	fn_stat %>%
+# 	ggplot(aes(x = fp / 100, y = `false negative predicted` )) + geom_point()  +
+# 	geom_abline(intercept = 0, slope = 1) +
+# 	xlab("False positive aimed") +
+# 	ylab("False negative predicted") +
+# 	my_theme
 
 # ROC
-p3 =
+(
 	stats %>%
 	mutate(strategy = "2-steps") %>%
 	bind_rows(stats_1_step_model %>% mutate(strategy = "1-step")) %>%
 	arrange(FP_rate, TP_rate) %>%
 	ggplot(aes(y = TP_rate, x = FP_rate, color = strategy)) +
-	geom_abline(intercept = 0, slope = 1, color="grey", linestyle="dotted") +
+	geom_abline(intercept = 0, slope = 1, color="grey", linetype = "dashed") +
 	geom_line() +
+	geom_point(size=1) +
+	scale_color_brewer(palette="Set1")+
 	xlab("False positive rate") +
 	ylab("True positive rate") +
 	my_theme
-
-# Compose plots
-cowplot::plot_grid(plotlist = list(p1, p2, p3), align = "v", ncol = 3, axis="b", rel_widths = 1 ) %>%
-
-	# Save plots
+) %>%
 	ggsave(
-		"dev/fp_fn_ROC.pdf",
+		"dev/fp_tp_ROC.pdf",
 		plot = .,
 		useDingbats=FALSE,
 		units = c("mm"),
-		width = 183
+		width = 183/3
 	)
+
+# # Compose plots
+# p = p1 + p2 + p3
+# cowplot::plot_grid(plotlist = list(p1, p2, p3), align = "v", ncol = 3, axis="b", rel_widths = 1 ) %>%
+#
+# 	# Save plots
+# 	ggsave(
+# 		"dev/fp_fn_ROC.pdf",
+# 		plot = .,
+# 		useDingbats=FALSE,
+# 		units = c("mm"),
+# 		width = 183
+# 	)
 
 
 #
