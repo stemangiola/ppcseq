fd8d4c3bd55148de4392457baf89c22c1cf7f6fb
stemangiola <mangiolastefano@gmail.com>
2020-06-17 10:13:16 +1000

    update dev scripts

---
+++ dev/run_all_datasets.sh
@@ -0,0 +1,8 @@
+module load R/3.6.1
+Rscript dev/CD8_tubercolosis.R &
+Rscript dev/arabidopsis.R &
+Rscript dev/glioblastoma.R &
+Rscript dev/muscle.R &
+Rscript dev/fat.R &
+Rscript dev/lipoma.R &
+jobs
