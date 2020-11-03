8a949758e0d33c0e2757e11e002d3e25e5254e0c
stemangiola <mangiolastefano@gmail.com>
2020-11-03 17:16:09 +1100

    update model for efficiency

--- R/ppcseq.R
+++ R/ppcseq.R
@@ -53,7 +53,7 @@ identify_outliers = function(.data,
 														 how_many_negative_controls = 500,
 
 														 approximate_posterior_inference = T,
-														 approximate_posterior_analysis = NULL,
+														 approximate_posterior_analysis = T,
 														 draws_after_tail = 10,
 
 														 save_generated_quantities = F,
@@ -64,7 +64,8 @@ identify_outliers = function(.data,
 														 tol_rel_obj = 0.01,
 														 just_discovery = F,
 														 seed = sample(1:99999, size = 1),
-														 adj_prob_theshold_2 = NULL
+														 adj_prob_theshold_2 = NULL,
+														 return_fit = FALSE
 ) {
 	# Prepare column same enquo
 	.sample = enquo(.sample)
@@ -281,23 +282,31 @@ identify_outliers = function(.data,
 	merge_results(
 
 		# Calculate CI 2 for discovery for plotting
-		res_discovery %>%
-			left_join(
-				(.) %>%
-					attr("fit") %>%
-					fit_to_counts_rng_approximated(adj_prob_theshold_2, how_many_posterior_draws_2, truncation_compensation = 0.7352941, cores) %>%
-					select(S, G, .lower_1 = .lower, .upper_1 = .upper)
-			),
-		res_test, formula,
+		res_discovery,
+
+		# %>%
+		# 	left_join(
+		# 		(.) %>%
+		# 			attr("fit") %>%
+		# 			fit_to_counts_rng_approximated(adj_prob_theshold_2, how_many_posterior_draws_2, truncation_compensation = 0.7352941, cores) %>%
+		# 			select(S, G, .lower_1 = .lower, .upper_1 = .upper)
+		# 	),
+
+		res_test,
+		formula,
 		!!.transcript,
 		!!.abundance,
 		!!.sample,
 		do_check_only_on_detrimental
 	) %>%
 
-		# Add fit attribute if any
-		add_attr(res_discovery %>% attr("fit"), "fit 1") %>%
-		add_attr(res_test %>% attr("fit"), "fit 2") %>%
+		# If return_fit
+		when(
+			return_fit ~ (.) %>%
+				add_attr(res_discovery %>% attr("fit"), "fit 1") %>%
+				add_attr(res_test %>% attr("fit"), "fit 2"),
+			~ (.)
+		) %>%
 
 		# Add total draws
 		add_attr(res_test %>% attr("total_draws"), "total_draws")
@@ -547,7 +556,7 @@ do_inference = function(my_df,
 
 		ifelse_pipe(
 			approximate_posterior_analysis,
-			~ .x %>% fit_to_counts_rng_approximated(adj_prob_theshold, how_many_posterior_draws * 10, truncation_compensation, cores),
+			~ .x %>% fit_to_counts_rng_approximated(adj_prob_theshold, how_many_posterior_draws, truncation_compensation, cores, how_many_to_check),
 			~ .x %>% fit_to_counts_rng(adj_prob_theshold)
 		) %>%
 
