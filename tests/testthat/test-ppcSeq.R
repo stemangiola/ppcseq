48ad3e7b24ce19d6b14ef62d4710042097c8b924
stemangiola <mangiolastefano@gmail.com>
2021-03-17 12:07:39 +1100

    update repo for CHECK
    Former-commit-id: fb7917a8fe8c56b87386834c6b4b948ee7868048

--- tests/testthat/test-ppcSeq.R
+++ tests/testthat/test-ppcSeq.R
@@ -4,7 +4,7 @@ test_that("VB post approx no correction",{
 
   res =
     ppcseq::counts %>%
-    dplyr::mutate(  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), T, F) ) %>%
+    dplyr::mutate(  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), TRUE, FALSE) ) %>%
     ppcseq::identify_outliers(
       formula = ~ Label,
       sample, symbol, value,
@@ -12,10 +12,10 @@ test_that("VB post approx no correction",{
       .do_check  = is_significant,
       percent_false_positive_genes = 1,
       tol_rel_obj = 0.01,
-      approximate_posterior_inference = T,
-      approximate_posterior_analysis = T,
+      approximate_posterior_inference =TRUE,
+      approximate_posterior_analysis =TRUE,
       how_many_negative_controls = 50,
-      cores=1,pass_fit = T
+      cores=1,pass_fit = TRUE
     )
 
   expect_equal(
@@ -30,17 +30,17 @@ test_that("VB post approx yes correction",{
 
   res =
     ppcseq::identify_outliers(
-      dplyr::mutate(ppcseq::counts,  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), T, F) ),
+      dplyr::mutate(ppcseq::counts,  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), TRUE, FALSE) ),
       formula = ~ Label,
       sample, symbol, value,
       .significance = PValue,
       .do_check  = is_significant,
       percent_false_positive_genes = 1,
       tol_rel_obj = 0.01,
-      approximate_posterior_inference = T,
-      approximate_posterior_analysis = T,
+      approximate_posterior_inference =TRUE,
+      approximate_posterior_analysis =TRUE,
       how_many_negative_controls = 50,
-      cores=1,pass_fit = T
+      cores=1,pass_fit = TRUE
     )
 
   expect_equal(
@@ -55,15 +55,15 @@ test_that("VB post full",{
 
   res =
     ppcseq::identify_outliers(
-      dplyr::mutate(ppcseq::counts,  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), T, F) ),
+      dplyr::mutate(ppcseq::counts,  is_significant = ifelse(symbol %in% c("SLC16A12", "CYP1A1", "ART3"), TRUE, FALSE) ),
       formula = ~ Label,
       sample, symbol, value,
       .significance = PValue,
       .do_check  = is_significant,
       percent_false_positive_genes = 1,
       tol_rel_obj = 0.01,
-      approximate_posterior_inference = T,
-      approximate_posterior_analysis = F,
+      approximate_posterior_inference =TRUE,
+      approximate_posterior_analysis = FALSE,
       how_many_negative_controls = 50,
       cores=1
     )
